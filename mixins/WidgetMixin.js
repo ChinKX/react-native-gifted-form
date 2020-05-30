b423244b985bd6da361f4a010d81ff6a2aefe8af
ChinKX <40538074+ChinKX@users.noreply.github.com>
2020-05-30 16:35:25 +0800

    Fixed 'Image.resizeMode.contain' error

--- mixins/WidgetMixin.js
+++ mixins/WidgetMixin.js
@@ -219,7 +219,7 @@ module.exports = {
       return (
         <Image
           style={this.getStyle('rowImage')}
-          resizeMode={Image.resizeMode.contain}
+          resizeMode={'contain'}// Fixed bug
           source={imageSrc}
         />
       );
@@ -231,7 +231,7 @@ module.exports = {
       return (
         <Image
           style={this.getStyle('rowImage')}
-          resizeMode={Image.resizeMode.contain}
+          resizeMode={'contain'}// Fixed bug
           source={this.props.image}
         />
       );
